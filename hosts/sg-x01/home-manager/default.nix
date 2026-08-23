bfec34ca023f187ecbcb8def16adf56f81ca1920
nemowang <nemowang@outlook.com>
2026-08-23 17:14:25 +0000

    sg-x01: git SSH 签名使用 nixpkgs 的 ssh-keygen

--- hosts/sg-x01/home-manager/default.nix
+++ hosts/sg-x01/home-manager/default.nix
@@ -1,3 +1,7 @@
-{...}: {
+{pkgs, ...}: {
   home.stateVersion = "25.11";
+
+  # OL8 自带的 OpenSSH 8.0 不支持 ssh-keygen -Y，git SSH 签名改用
+  # nixpkgs 的 ssh-keygen；ssh 传输仍走系统 OpenSSH。
+  programs.git.settings.gpg.ssh.program = "${pkgs.openssh}/bin/ssh-keygen";
 }
